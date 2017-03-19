63176753ee80349e900ed32957fe393b64a53e49
Ashishravi <ashish.ravik@gmail.com>
2017-03-19 23:51:46 +0530

    modified add challan and pdf feature added

--- add_challan_sql.php
+++ add_challan_sql.php
@@ -8,12 +8,13 @@ if(isset($_POST['submit_row']))
  $item_quantity=$_POST['item_quantity'];
  $app_price=$_POST['app_price'];
  $total_price=$_POST['total_price'];
+ $challan_id=time();
     
- for($i=0;$i<count($name);$i++)
+ for($i=0;$i<count($item_code);$i++)
  {
-  if($name[$i]!="" && $age[$i]!="" && $job[$i]!="")
+  if($item_code[$i]!="" && $item_description[$i]!="" && $item_quantity[$i]!="")
   { 
-      $sql = "insert into challan_items values('$item_code[$i]','$item_description[$i]','$item_quantity[$i]')";
+      $sql = "insert into challan_items (challan_id,item_code,item_description,item_quantity,app_price,total_price) values('$challan_id','$item_code[$i]','$item_description[$i]','$item_quantity[$i]','$app_price[$i]','$total_price[$i]')";
        mysqli_query($con, $sql);
   }
  }
